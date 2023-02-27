1b820740d8180366225f3cb3a76974ff34b9f088
500ping <ndt512x@gmail.com>
2023-02-27 09:50:25 +0700

    first init

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='demo_python_lib',
+    version='0.0.11',
+    description='Demo python lib from git',
+    url='https://github.com/500ping/demo_python_lib',
+    author='Thang Nguyen',
+    author_email='ndt512x@gmail.com',
+    packages=['demo_python_lib'],
+    # install_requires=[
+    #     'requests>=2.25.1',
+    #     'tqdm>=4.59.0'
+    # ]
+)
